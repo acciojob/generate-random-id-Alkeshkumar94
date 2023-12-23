57140506d2067ea175eeacefeec51858d2781355
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2023-12-23 14:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function makeid(l) {
   // write your code here
+	function valcode(){
+		let num=parseInt(Math.random()*100);
+		if((48<=num && num<=57) || (65<=num && num<=90) || (97<=num && num<=99)){
+			return num;
+		}
+		return 100+parseInt(num/5);
+	}
+		let str="";
+	
+	for(let i=1;i<=l;i++){
+		str+=String.fromCharCode(valcode());
+	}
+	return str;
 }
 
 // Do not change the code below.
